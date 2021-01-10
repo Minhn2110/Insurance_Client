52314f7bc359f744dd6f057556d5d16f915fb6ce
minhnha <minhnha@smartosc.com>
2021-01-10 23:49:37 +0700

    update

---
+++ src/app/components/model/claim.ts
@@ -0,0 +1,13 @@
+export class Claim {
+  userId: Number;
+  descriptionOfAccident: String;
+  numberPlate: String;
+  phoneNumber: String;
+
+  contractId: Number;
+  witnessesName: String;
+  witnessesPhoneNumber: String;
+  Image: File;
+
+
+}
